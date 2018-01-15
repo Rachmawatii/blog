77cbcf0ea96196bd6c28dc9d79932019b30d87fe
Rachmawatii <rachma1562@gmail.com>
2018-01-14 19:41:45 -1200

    Rahmawati

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
                 <div class="navbar-header">
 
                     <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
                         <span class="sr-only">Toggle Navigation</span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
@@ -36,26 +36,22 @@
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
-                        <li><a href="{{route('category.index')}}">Categories</a></li>
-                        <li><a href="{{route('post.index')}}">Posts</a></li>
-                        <li><a href="{{route('author.index')}}">Authors</a></li>
-                        <li><a href="{{route('publisher.index')}}">Publishers</a></li>
-                        <li><a href="{{route('book.index')}}">Books</a></li>
+                        &nbsp;
                     </ul>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
-                        @if (Auth::guest())
+                        @guest
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
                             <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
-                                <ul class="dropdown-menu" role="menu">
+                                <ul class="dropdown-menu">
                                     <li>
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
@@ -69,7 +65,7 @@
                                     </li>
                                 </ul>
                             </li>
-                        @endif
+                        @endguest
                     </ul>
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,6 @@ Route::group(['middleware' => 'auth'], function(){
 		return view('welcome');
 		});
 	});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
